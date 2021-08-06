de5e5b83672f457e38a76c5f6239d7473ae07056
Ruslan <sabbath9993@gmail.com>
2021-08-06 16:01:39 +0300

    adding properties

--- src/redux/reducers/searchReducer.js
+++ src/redux/reducers/searchReducer.js
@@ -1,5 +1,10 @@
 const initialState = {
   value: "",
+  query: "",
+  startIndex: 0,
+  sorting: "relevance",
+  subject: "all",
+  loading: false,
 };
 
 export const searchReducer = (state = initialState, { type, payload }) => {
